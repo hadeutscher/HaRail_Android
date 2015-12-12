f8e734a32ccfe63ec98061acd7f790f7bfc2c32d
haha01haha01 <haha01haha01@gmail.com>
2015-12-12 17:21:05 +0200

    All working, revert settings to local armeabi building

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := HaRail
 LOCAL_SRC_FILES := HaRail.cpp HaRail/HaRail/Graph.cpp HaRail/HaRail/GTFSReader.cpp HaRail/HaRail/GTFSDataSource.cpp HaRail/HaRail/IDataSource.cpp HaRail/HaRail/lib_api.cpp HaRail/HaRail/StringTokenizer.cpp HaRail/HaRail/Utils.cpp
-LOCAL_C_INCLUDES += D:\clib\boost
+LOCAL_C_INCLUDES += ./
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi-v7a,x86_64,x86
+APP_ABI := armeabi-v7a
 APP_PLATFORM := android-21
 NDK_TOOLCHAIN_VERSION := 4.9
 
